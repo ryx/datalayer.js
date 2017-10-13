d91dcb64af76141f3c27687afe5aa7156cccf3c2
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-10-13 16:43:42 +0200

    ported remaining utils spec tests, fixed some more things

--- src/lib/utils.spec.js
+++ src/lib/utils.spec.js
@@ -4,7 +4,7 @@ import { JSDOM } from 'jsdom';
 
 // stub dependencies
 const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
-const window = td.replace('./lib/window', dom.window);
+const window = td.replace('./window', dom.window);
 
 import('./utils').then((module) => {
   const utils = module.default;
@@ -44,12 +44,69 @@ import('./utils').then((module) => {
 
   test('method queue: should throw an error on methods that are not available in API object', (t) => {
     const { contextObj, apiObj } = setupMQP();
+
     t.throws(
       () => {
         utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
         contextObj._testq.push(['kaboom', 'bazinga!']);
       },
       'method "kaboom" not found'
+    );
+
+    t.end();
+  });
+
+  test('collectMetadata', (t) => {
+    const data = { stringProp: 'hello', numberProp: 42, numberProp2: 76.54 };
+    window.document.querySelector('body').innerHTML = `<meta name="dal:data" content='${JSON.stringify(data)}' />`;
+    t.deepEqual(
+      utils.collectMetadata('dal:data', () => {}),
+      data,
+      'should return an object with an aggregation of all provided metatags\' data'
+    );
+
+    const innerData = { innerProp: 'foo' };
+    window.document.querySelector('body').innerHTML =
+      `<meta name="dal:data" content='${JSON.stringify(data)}' />` +
+      `<div id="data-container"><meta name="dal:data" content='${JSON.stringify(innerData)}' /></div>`;
+    t.deepEqual(
+      utils.collectMetadata('dal:data', () => {}, '#data-container'),
+      innerData,
+      'should accept a different parent context as argument and collect metatags only within that element'
+    );
+
+    t.notOk(
+      utils.collectMetadata('dal:data', () => {}, '#non-existent'),
+      'return false if context cannot be found',
+    );
+
+    const collectedData = [];
+    window.document.querySelector('body').innerHTML =
+      '<meta name="dal:data" content=\'{"foo":"bar"}\' />' +
+      '<meta name="dal:data" content=\'{"foo":"bar"}\' />' +
+      '<meta name="dal:data" content=\'{"foo":"bar"}\' />';
+    utils.collectMetadata('dal:data', (err, element, content) => collectedData.push(content));
+    t.deepEqual(
+      collectedData,
+      [{ foo: 'bar' }, { foo: 'bar' }, { foo: 'bar' }],
+      'should fire a callback for each collected metatag and pass element and JSON.parse\'d content',
+    );
+
+    window.document.querySelector('body').innerHTML =
+      '<meta name="dal:data" content=\'{"string1":"hello","number1":42}\' />' +
+      '<meta name="dal:data" content=\'{"string2":"foo","number2":76}\' />' +
+      '<meta name="dal:data" content=\'{"string3":"bar","number3":777}\' />';
+    t.deepEqual(
+      utils.collectMetadata('dal:data', () => {}),
+      {
+        string1: 'hello',
+        number1: 42,
+        number2: 76,
+        string2: 'foo',
+        number3: 777,
+        string3: 'bar',
+      },
+      'should return an object with an aggregation of all provided metatags\' data',
     );
 
     t.end();
@@ -174,107 +231,4 @@ import('./utils').then((module) => {
 
     t.end();
   });
-
-  test('collectMetadata', (t) => {
-    const data = { stringProp: 'hello', numberProp: 42, numberProp2: 76.54 };
-    window.document.querySelector('body').innerHTML = `<meta name="dal:data" content='${JSON.stringify(data)}' />`;
-    t.deepEqual(
-      utils.collectMetadata('dal:data', () => {}),
-      data,
-      'should return an object with an aggregation of all provided metatags\' data'
-    );
-  });
 });
-
-/*
-describe('odl/lib/utils', () => {
-  let [utils, elementSpy, windowSpy, loggerSpy] = [];
-
-  sinon.log = message => console.log(message);
-
-  beforeEach(() => {
-    // spies
-    elementSpy = {
-      tagName: 'foo',
-      getAttribute: sinon.stub(),
-      setAttribute: sinon.spy(),
-      hasAttribute: sinon.stub().returns(false),
-    };
-    windowSpy = {
-      location: { search: '' },
-      require: sinon.spy(),
-      document: {
-        querySelector: sinon.stub().returns(elementSpy),
-        querySelectorAll: sinon.stub().returns([elementSpy, elementSpy, elementSpy]),
-      },
-    };
-    loggerSpy = { log: sinon.spy(), warn: sinon.spy(), error: sinon.spy() };
-    // register mocks
-    mockModule(System, 'odl/lib/globals/window', windowSpy);
-    mockModule(System, 'odl/lib/logger', () => loggerSpy);
-    // clear module first
-    System.delete(System.normalizeSync('odl/lib/utils'));
-    return System.import('odl/lib/utils').then((m) => {
-      utils = m.default;
-    });
-  });
-
-  describe('collectMetadata', () => {
-    it('should query the global context for metatags with the given name', () => {
-      utils.collectMetadata('odl:data', () => {});
-      sinon.assert.calledWith(windowSpy.document.querySelectorAll, 'meta[name="odl:data"]');
-    });
-
-    it('should accept a different parent context as argument and collect metatags only within that element', () => {
-      const contextSpy = { querySelectorAll: sinon.stub().returns([elementSpy]) };
-      windowSpy.document.querySelector.returns(contextSpy);
-      utils.collectMetadata('odl:data', () => {}, contextSpy);
-      sinon.assert.calledWith(contextSpy.querySelectorAll, 'meta[name="odl:data"]');
-    });
-
-    it('should log an error and return false if context cannot be found', () => {
-      windowSpy.document.querySelector.returns(null);
-      assert.isFalse(utils.collectMetadata('odl:data', () => {}, 'notfound'));
-      sinon.assert.calledWith(loggerSpy.error, sinon.match('collectMetadata: context with selector "notfound" not found'));
-    });
-
-    it('should fire a callback for each collected metatag and pass element and JSON.parse\'d content', () => {
-      const data = [];
-      elementSpy.getAttribute.returns('{"foo":"bar"}');
-      utils.collectMetadata('odl:data', (err, element, content) => data.push(content));
-      assert.deepEqual(data, [{ foo: 'bar' }, { foo: 'bar' }, { foo: 'bar' }]);
-    });
-
-    it('should fire a callback for each collected metatag and pass an error if JSON.parse failed', () => {
-      elementSpy.getAttribute.returns('$this_is_no_json!!');
-      utils.collectMetadata('odl:data', (err) => {
-        assert.include(err, 'collectMetadata: parse error');
-      });
-    });
-
-    it('should return an object with an aggregation of all provided metatags\' data', () => {
-      const makeElement = (data) => {
-        return {
-          getAttribute: sinon.stub().returns(data),
-          hasAttribute: sinon.stub().returns(true),
-          setAttribute: sinon.spy(),
-        };
-      };
-      const element1 = makeElement('{"string1":"hello","number1":42}');
-      const element2 = makeElement('{"string2":"foo","number2":76}');
-      const element3 = makeElement('{"string3":"bar","number3":777}');
-      windowSpy.document.querySelectorAll.returns([element1, element2, element3]);
-      assert.deepEqual(utils.collectMetadata('odl:data', () => {}), {
-        string1: 'hello',
-        string2: 'foo',
-        string3: 'bar',
-        number1: 42,
-        number2: 76,
-        number3: 777,
-      });
-    });
-  });
-
-  // parent context
-});
-*/
