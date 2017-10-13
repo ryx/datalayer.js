3d009b905c1cd59a7a5b06ec61ea4aa2ad7a0c93
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-10-13 22:41:04 +0200

    use subtests in utils module, too

--- src/lib/utils.spec.js
+++ src/lib/utils.spec.js
@@ -9,110 +9,131 @@ const window = td.replace('./window', dom.window);
 import('./utils').then((module) => {
   const utils = module.default;
 
-  const setupMQP = () => {
-    const contextObj = { _testq: [] };
-    const apiObj = {
-      method1: td.function('method1'),
-      method2: td.function('method2'),
+  test('method queue:', (t) => {
+    const setupMQP = () => {
+      const contextObj = { _testq: [] };
+      const apiObj = {
+        method1: td.function('method1'),
+        method2: td.function('method2'),
+      };
+      return { contextObj, apiObj };
     };
-    return { contextObj, apiObj };
-  };
 
-  test('method queue: should recognize and execute methods that have been added BEFORE initialization', (t) => {
-    const { contextObj, apiObj } = setupMQP();
-    contextObj._testq.push(['method1', 123, 'foo']);
-    contextObj._testq.push(['method2', { data: 'test' }]);
-    utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
+    t.test('should recognize and execute methods that have been added BEFORE initialization', (st) => {
+      const { contextObj, apiObj } = setupMQP();
+      contextObj._testq.push(['method1', 123, 'foo']);
+      contextObj._testq.push(['method2', { data: 'test' }]);
+      utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
 
-    td.verify(apiObj.method1(123, 'foo'));
-    td.verify(apiObj.method2({ data: 'test' }));
+      td.verify(apiObj.method1(123, 'foo'));
+      td.verify(apiObj.method2({ data: 'test' }));
 
-    t.end();
+      st.end();
+    });
+
+    t.test('should recognize and execute methods that have been added AFTER initialization', (st) => {
+      const { contextObj, apiObj } = setupMQP();
+      utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
+      contextObj._testq.push(['method1', 123, 'foo']);
+      contextObj._testq.push(['method2', { data: 'test' }]);
+
+      td.verify(apiObj.method1(123, 'foo'));
+      td.verify(apiObj.method2({ data: 'test' }));
+
+      st.end();
+    });
+
+    t.test('should throw an error on methods that are not available in API object', (st) => {
+      const { contextObj, apiObj } = setupMQP();
+
+      st.throws(
+        () => {
+          utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
+          contextObj._testq.push(['kaboom', 'bazinga!']);
+        },
+        /method "kaboom" not found/gi,
+        'should throw error on missing method'
+      );
+
+      st.end();
+    });
   });
 
-  test('method queue: should recognize and execute methods that have been added AFTER initialization', (t) => {
-    const { contextObj, apiObj } = setupMQP();
-    utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
-    contextObj._testq.push(['method1', 123, 'foo']);
-    contextObj._testq.push(['method2', { data: 'test' }]);
+  test('collectMetadata:', (t) => {
+    t.test('should return an object with an aggregation of all provided metatags\' data', (st) => {
+      const data = { stringProp: 'hello', numberProp: 42, numberProp2: 76.54 };
+      window.document.querySelector('body').innerHTML = `<meta name="dal:data" content='${JSON.stringify(data)}' />`;
 
-    td.verify(apiObj.method1(123, 'foo'));
-    td.verify(apiObj.method2({ data: 'test' }));
+      st.deepEqual(
+        utils.collectMetadata('dal:data', () => {}),
+        data,
+        'collected metadata is equal to original data'
+      );
+      st.end();
+    });
 
-    t.end();
-  });
+    t.test('should accept a different parent context as argument and collect metatags only within that element', (st) => {
+      const data = { outerProp: 'foo' };
+      const innerData = { innerProp: 'bar' };
+      window.document.querySelector('body').innerHTML =
+        `<meta name="dal:data" content='${JSON.stringify(data)}' />` +
+        `<div id="data-container"><meta name="dal:data" content='${JSON.stringify(innerData)}' /></div>`;
 
-  test('method queue: should throw an error on methods that are not available in API object', (t) => {
-    const { contextObj, apiObj } = setupMQP();
-
-    t.throws(
-      () => {
-        utils.createMethodQueueHandler(contextObj, '_testq', apiObj);
-        contextObj._testq.push(['kaboom', 'bazinga!']);
-      },
-      'method "kaboom" not found'
-    );
-
-    t.end();
-  });
-
-  test('collectMetadata', (t) => {
-    const data = { stringProp: 'hello', numberProp: 42, numberProp2: 76.54 };
-    window.document.querySelector('body').innerHTML = `<meta name="dal:data" content='${JSON.stringify(data)}' />`;
-    t.deepEqual(
-      utils.collectMetadata('dal:data', () => {}),
-      data,
-      'should return an object with an aggregation of all provided metatags\' data'
-    );
-
-    const innerData = { innerProp: 'foo' };
-    window.document.querySelector('body').innerHTML =
-      `<meta name="dal:data" content='${JSON.stringify(data)}' />` +
-      `<div id="data-container"><meta name="dal:data" content='${JSON.stringify(innerData)}' /></div>`;
-    t.deepEqual(
-      utils.collectMetadata('dal:data', () => {}, '#data-container'),
-      innerData,
-      'should accept a different parent context as argument and collect metatags only within that element'
-    );
+      st.deepEqual(
+        utils.collectMetadata('dal:data', () => {}, '#data-container'),
+        innerData,
+        'collected metadata contains only inner data'
+      );
+      st.end();
+    });
 
     t.notOk(
       utils.collectMetadata('dal:data', () => {}, '#non-existent'),
       'return false if context cannot be found',
     );
 
-    const collectedData = [];
-    window.document.querySelector('body').innerHTML =
-      '<meta name="dal:data" content=\'{"foo":"bar"}\' />' +
-      '<meta name="dal:data" content=\'{"foo":"bar"}\' />' +
-      '<meta name="dal:data" content=\'{"foo":"bar"}\' />';
-    utils.collectMetadata('dal:data', (err, element, content) => collectedData.push(content));
-    t.deepEqual(
-      collectedData,
-      [{ foo: 'bar' }, { foo: 'bar' }, { foo: 'bar' }],
-      'should fire a callback for each collected metatag and pass element and JSON.parse\'d content',
-    );
+    t.test('should fire a callback for each collected metatag and pass element and JSON.parse\'d content', (st) => {
+      const collectedData = [];
+      window.document.querySelector('body').innerHTML =
+        '<meta name="dal:data" content=\'{"foo":"bar"}\' />' +
+        '<meta name="dal:data" content=\'{"foo":"bar"}\' />' +
+        '<meta name="dal:data" content=\'{"foo":"bar"}\' />';
 
-    window.document.querySelector('body').innerHTML =
-      '<meta name="dal:data" content=\'{"string1":"hello","number1":42}\' />' +
-      '<meta name="dal:data" content=\'{"string2":"foo","number2":76}\' />' +
-      '<meta name="dal:data" content=\'{"string3":"bar","number3":777}\' />';
-    t.deepEqual(
-      utils.collectMetadata('dal:data', () => {}),
-      {
-        string1: 'hello',
-        number1: 42,
-        number2: 76,
-        string2: 'foo',
-        number3: 777,
-        string3: 'bar',
-      },
-      'should return an object with an aggregation of all provided metatags\' data',
-    );
+      utils.collectMetadata('dal:data', (err, element, content) => collectedData.push(content));
+
+      st.deepEqual(
+        collectedData,
+        [{ foo: 'bar' }, { foo: 'bar' }, { foo: 'bar' }],
+        'should contain the parsed JSON from the metatags as array',
+      );
+      st.end();
+    });
+
+    t.test('should return an object with an aggregation of all provided metatags\' data', (st) => {
+      window.document.querySelector('body').innerHTML =
+        '<meta name="dal:data" content=\'{"string1":"hello","number1":42}\' />' +
+        '<meta name="dal:data" content=\'{"string2":"foo","number2":76}\' />' +
+        '<meta name="dal:data" content=\'{"string3":"bar","number3":777}\' />';
+
+      t.deepEqual(
+        utils.collectMetadata('dal:data', () => {}),
+        {
+          string1: 'hello',
+          number1: 42,
+          number2: 76,
+          string2: 'foo',
+          number3: 777,
+          string3: 'bar',
+        },
+        'should contain the aggregated data',
+      );
+      st.end();
+    });
 
     t.end();
   });
 
-  test('extend', (t) => {
+  test('extend:', (t) => {
     t.plan(3);
 
     t.deepEqual(
@@ -231,4 +252,6 @@ import('./utils').then((module) => {
 
     t.end();
   });
+
+  td.reset();
 });
