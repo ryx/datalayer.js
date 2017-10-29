7c23a5add87c4b76863fa71d34924050b9050d69
Rico Pfaus <rico.pfaus@kaufhof.de>
2017-10-29 22:02:23 +0100

    added license notice to header

--- src/datalayer.js
+++ src/datalayer.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2016-present, Rico Pfaus
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
 import window from './lib/window';
 import utils from './lib/utils';
 import cookie from './lib/cookie';
