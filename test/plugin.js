61a52465ba22d2fa25ad3f5b04dab3c61fbe711e
Rico Pfaus <rico.pfaus@kaufhof.de>
2018-04-12 10:51:56 +0200

    improved testing playground

--- test/plugin.js
+++ test/plugin.js
@@ -11,14 +11,15 @@ import { JSDOM } from 'jsdom';
 
 const mockdata = {
   page: { type: 'test', name: 'Test' },
+  view: { type: 'homepage', name: 'Homepage' },
   site: { id: 'testing' },
   user: {},
 };
 const dom = new JSDOM(`<!DOCTYPE html>
 <html>
   <body>
-  <meta name="dtlr:data" content='${JSON.stringify(mockdata)}' />
-  <div id="annotationsDiv" data-dtlr-event-view='{"name":"foo","data":{"bar":123}}'>Foo</div>
+  <meta name="d7r:data" content='${JSON.stringify(mockdata)}' />
+  <div id="annotationsDiv" data-d7r-event-view='{"name":"foo","data":{"bar":123}}'>Foo</div>
   </body>
 </html>
 `);
@@ -41,31 +42,53 @@ const annotations = require('../extensions/annotations/annotations').default;
 
 // Plugin test
 /* eslint-disable class-methods-use-this */
-export default class ExamplePlugin extends Plugin {
+
+class GlobalPlugin extends Plugin {
   constructor(config = {}) {
-    super('my-plugin-id', config);
+    super('my-global-plugin', config);
   }
 
   handleInit() {
     // setup third party resources
   }
 
-  handleActivate(data) {
-    return data.page.type === 'test';
+  handleEvent(name, data) {
+    switch (name) {
+      case 'pageload':
+        console.log('GlobalPlugin.handleEvent: Page loaded', data);
+        break;
+      case 'say-hello':
+        console.log(`GlobalPlugin.handleEvent: Hello from ${this.config.testProp}, caught "${name}"`, data);
+        break;
+      default:
+        console.log(`GlobalPlugin.handleEvent: ${name}`, data);
+    }
+  }
+}
+
+class ConversionPlugin extends Plugin {
+  constructor(config = {}) {
+    super('my-conversion-plugin', config);
+  }
+
+  handleInit() {
+    // setup third party resources
+  }
+
+  shouldActivate(data) {
+    return data.page.type === 'conversion';
   }
 
   handleEvent(name, data) {
     switch (name) {
       case 'pageload':
-        console.log('ExamplePlugin.handleEvent: Page loaded');
+        console.log('ConversionPlugin.handleEvent: Page loaded', data);
         break;
       case 'say-hello':
-        console.log(`ExamplePlugin.handleEvent: Hello from ${this.getId()}\n` +
-          `My event data: ${data}\n` +
-          `My testProp from config: ${this.config.testProp}`);
+        console.log(`ConversionPlugin.handleEvent: Hello from ${this.config.testProp}, caught "${name}"`, data);
         break;
       default:
-        console.log(`ExamplePlugin.handleEvent: ${name}`, data);
+        console.log(`ConversionPlugin.handleEvent: ${name}`, data);
     }
   }
 }
@@ -76,16 +99,26 @@ export default class ExamplePlugin extends Plugin {
  * from the backend application, using the metadata extension.
  */
 datalayer
-  .use(metadata({ metaPrefix: 'dtlr:' }))
-  .use(annotations({ attributePrefix: 'dtlr' }))
+  .use(metadata({ metaPrefix: 'd7r:' }))
+  .use(annotations({ attributePrefix: 'd7r' }))
   .initialize({
     // data: mockdata,
     plugins: [
-      new ExamplePlugin({ testProp: 'myplugin-private-number-1' }),
-      new ExamplePlugin({ testProp: 'myplugin-private-number-2' }),
+      new GlobalPlugin({ testProp: 'myplugin-private-number-1' }),
+      new ConversionPlugin({ testProp: 'myplugin-private-number-2' }),
     ],
   })
   .whenReady()
   .then(() => {
+    // should only get caught by global plugin
     datalayer.broadcast('say-hello');
+
+    // for MPA's we would broadcast a "view-loaded" event with the globally available D7rPageData here
+    // ...
+
+    // for SPA's we might indicate a view change from somewhere inside the data flow (e.g. Redux)
+    datalayer.broadcast('view-loaded', {
+      type: 'checkout-completed',
+      name: 'Thank You Page',
+    });
   });
