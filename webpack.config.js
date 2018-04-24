fe37adb4295573416849e879e99d097ac3307f72
Rico Pfaus <ricopfaus@gmail.com>
2018-04-24 15:57:07 +0200

    switched from rollup to webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'datalayer.umd.js',
+    library: 'datalayerjs',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
