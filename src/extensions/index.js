e2221f4a5237d6ff8e1b891600973fc65b5ffc78
Rico Pfaus <ricopfaus@gmail.com>
2018-09-14 14:34:35 +0200

    #2 fixed logger export statement

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -3,4 +3,4 @@ export { default as attribution } from './attribution/attribution';
 export { default as metadata } from './metadata/metadata';
 export { default as methodQueue } from './methodQueue/methodQueue';
 export { default as mutations } from './mutations/mutations';
-export { default as customLogger } from './customLogger/customLogger';
+export { default as logger } from './logger/logger';
