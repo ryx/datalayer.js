a2074d3455d334c5192e446ec7f8aefd03fb5846
Rico Pfaus <ricopfaus@gmail.com>
2018-11-16 16:13:27 +0100

    use new OOP-driven API in attribution extension

--- src/extensions/attribution/attribution.js
+++ src/extensions/attribution/attribution.js
@@ -3,9 +3,9 @@
  * based on a provided set of configuration rules.
  *
  * Attribution logic is based on our (internally used) attribution.js library,
- * that will follow as dedicated and polished OSS library somewhen, too.
+ * that will follow as a dedicated and polished OSS library somewhen, too.
  *
- * Usage: provide attribution config to extension and access attribution
+ * Usage: provide AttributionEngine to extension and access attribution
  * data and touchpoints via `data.attribution`
  *
  * Copyright (c) 2016 - present, Rico Pfaus
@@ -13,72 +13,51 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import {
-  initAttribution,
-  getAttributedChannel,
-  getChannelConfigByName,
-} from './_attribution';
-
 export default (config = {
-  cookieName: 'd7r:attribution',
-  touchpointLifetime: 60 * 60 * 24 * 30,
-  channels: [],
+  /**
+   * The associated AttributionEngine.
+   * @type {AttributionEngine | null}
+   */
+  engine: null,
 }) => class Attribution {
   constructor(datalayer) {
     this.datalayer = datalayer;
-    this.globalData = {};
   }
 
   /* eslint-disable class-methods-use-this */
   beforeInitialize() {
-    // init attribution lib and get current touchpoint
-    const recognizedTouchpoint = initAttribution(config);
-
-    // perform attribution handling based on config
-    const attributedChannel = getAttributedChannel();
-
-    // @FIXME: the following two blocks should be replaced with a more elegant
-    // interface for attribution.js - these "c" and "v" properties were never meant
-    // to be exposed to the outer world ...
-    // This is also currently hardcoded to a model with a single winning channel!
-
-    // attributed channel/touchpoint (should always be set if config is correct)
-    const touchpointCreditData = [];
-    if (attributedChannel) {
-      const attributedChannelConfig = getChannelConfigByName(attributedChannel.c);
-      if (attributedChannelConfig) {
-        touchpointCreditData.push({
-          touchpoint: {
-            id: attributedChannel.c,
-            label: attributedChannelConfig.label,
-            campaign: attributedChannel.v,
-          },
-          weight: 100,
-        });
-      } else {
-        this.datalayer.log('Error: attributedChannelConfig not available for ', attributedChannel);
-      }
+    if (!config.engine) {
+      throw new Error('d7r/extensions/attribution.js: extension expects an engine entry in config');
     }
+
+    // perform attribution handling based on provided engine
+    const recognizedTouchpoint = config.engine.execute();
 
     // currently recognized touchpoint (might be null, e.g. for "firstPageViewOnly" channels)
     let currentTouchpoint = null;
     if (recognizedTouchpoint) {
-      const recognizedTouchpointConfig = getChannelConfigByName(recognizedTouchpoint.c);
-      if (recognizedTouchpointConfig) {
-        currentTouchpoint = {
-          id: recognizedTouchpoint.c,
-          label: recognizedTouchpointConfig.label,
-          campaign: recognizedTouchpoint.v,
-        };
-      } else {
-        this.datalayer.log('Error: recognizedTouchpointConfig not available for ', recognizedTouchpoint);
-      }
+      const recognizedChannel = recognizedTouchpoint.getChannel();
+      currentTouchpoint = {
+        id: recognizedChannel.getId(),
+        label: recognizedChannel.getLabel(),
+        campaign: recognizedTouchpoint.getValue(),
+      };
     }
 
     // inject attribution data into datalayer
     return {
       attribution: {
-        credits: touchpointCreditData,
+        credits: config.engine.getAttributionItems().map((data) => {
+          const touchpoint = data.getTouchpoint();
+          return {
+            touchpoint: {
+              id: touchpoint.getChannel().getId(),
+              label: touchpoint.getChannel().getLabel(),
+              campaign: touchpoint.getValue(),
+            },
+            weight: data.getWeight(),
+          };
+        }),
         currentTouchpoint,
       },
     };
--- src/extensions/attribution/attribution.spec.js
+++ src/extensions/attribution/attribution.spec.js
@@ -1,23 +1,37 @@
 /* eslint-disable max-len, no-new */
 import attribution from './attribution';
 import datalayer from '../../datalayer';
+import {
+  AttributionEngine,
+  LastTouchAttributionModel,
+  URLMatchingChannel,
+} from './_attribution-new';
 
 const { jsdom } = global;
 
 describe('attribution', () => {
+  let [engine, model] = [];
+
+  beforeEach(() => {
+    model = new LastTouchAttributionModel();
+    engine = new AttributionEngine(model, [
+      new URLMatchingChannel('sea', 'SEA (js)', 'adword', 'adword', { canOverwrite: true }),
+    ]);
+  });
+
   describe('module', () => {
     it('should export a factory which returns the extension class', () => {
-      expect(typeof attribution()).toBe('function');
+      expect(typeof attribution({ engine })).toBe('function');
     });
   });
 
   describe('use', () => {
     it('should be loadable without errors by the datalayer', () => {
-      expect(() => datalayer.use(attribution())).not.toThrow();
+      expect(() => datalayer.use(attribution({ engine }))).not.toThrow();
     });
 
     it('should execute the beforeInitialize callback and extend the provided data when use\'d', async () => {
-      const extension = attribution();
+      const extension = attribution({ engine });
       datalayer.use(extension);
 
       datalayer.initialize();
@@ -33,7 +47,7 @@ describe('attribution', () => {
 
   describe('extension factory', () => {
     it('should return an empty `attribution` object on calling "beforeInitialize", when running extension without attribution config', () => {
-      const ExtensionContructor = attribution();
+      const ExtensionContructor = attribution({ engine });
       const instance = new ExtensionContructor(datalayer);
 
       const returnData = instance.beforeInitialize();
@@ -48,18 +62,7 @@ describe('attribution', () => {
 
     it('should return currentTouchpoint and credits according to provided config when calling "beforeInitialize"', () => {
       jsdom.reconfigure({ url: 'http://example.com?adword=my/campaign/123' });
-      const ExtensionContructor = attribution({
-        channels: [
-          {
-            name: 'sea',
-            label: 'SEA (js)',
-            type: 'match',
-            match: 'adword',
-            value: 'adword',
-            canOverwrite: true,
-          },
-        ],
-      });
+      const ExtensionContructor = attribution({ engine });
       const instance = new ExtensionContructor();
       const expectedTouchpointData = {
         id: 'sea',
